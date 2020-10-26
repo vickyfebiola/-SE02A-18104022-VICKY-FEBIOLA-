d0b3e05684365dc7794db9602e46e18923d87e20
vickyfebiola <vickyfebiola@gmail.com>
2020-10-26 21:12:48 +0700

    Pertemuan 3

---
+++ Pertemuan 3/HelloITTelkomPurwokerto2/app/src/main/java/com/vickyfebiola_18104022/helloittelkompurwokerto/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.vickyfebiola_18104022.helloittelkompurwokerto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
