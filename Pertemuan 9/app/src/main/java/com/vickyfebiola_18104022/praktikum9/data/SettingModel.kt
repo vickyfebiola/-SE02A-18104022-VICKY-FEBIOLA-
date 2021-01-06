c632187e17004e161c639559c0d4582407366372
vickyfebiola <vickyfebiola@gmail.com>
2021-01-06 14:59:38 +0700

    Pertemuan 9

---
+++ Pertemuan 9/app/src/main/java/com/vickyfebiola_18104022/praktikum9/data/SettingModel.kt
@@ -0,0 +1,15 @@
+package com.vickyfebiola_18104022.praktikum9.data
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+    data class SettingModel (
+        var name: String? = null,
+        var email: String? = null,
+        var age: Int = 0,
+        var phoneNumber: String? = null,
+        var alamat: String? = null,
+        var ttl: String? = null,
+        var isDarkTheme: Boolean = false
+    ): Parcelable
